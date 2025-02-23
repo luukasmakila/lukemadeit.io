1b78b81fbfb2febe4d74427cdd9ac35500d23c3b
luukasmakila <luukas.makila@kolumbus.fi>
2025-02-23 10:38:12 +0200

    chore: website layout components

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default function Blog() {
+  return (
+    <>
+      <h1>Blog page</h1>
+      <p>Blogs written by me about tech</p>
+    </>
+  )
+}
---
+++ src/app/gear/page.tsx
@@ -0,0 +1,8 @@
+export default function Gear() {
+  return (
+    <>
+      <h1>Gear page</h1>
+      <p>The gear I use for video & my setup</p>
+    </>
+  )
+}
